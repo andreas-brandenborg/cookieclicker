610ea2879e6392e2619bbb36bb7de4f3d9d979a1
Andreas <andreas.brandenborg@live.dk>
2024-10-23 11:26:15 +0200

    Lavede starten til hjemmeside

---
+++ main.js
@@ -0,0 +1,14 @@
+﻿
+//User score
+let userCurrentScore = 0;
+
+//Dom elements
+const userScoreHTML = document.querySelector(".score");
+const userClicks = document.querySelector(".cookie-btn")
+
+userClicks.addEventListener("click", () => {
+    userCurrentScore ++
+    console.log(userCurrentScore)
+    userScoreHTML.textContent = `${userCurrentScore}`
+})
+
